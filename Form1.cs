72c66a9964ad91e79205672701d2110d1728afba
duongthuy2205 <77923411+duongthuy2205@users.noreply.github.com>
2021-11-24 14:55:32 +0700

    Git

---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace AppQL_NhanSu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Login_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection(@"Data Source=TRONGNGHIA;Initial Catalog=QUANLYNHANSUDOANHNGHIEP;Integrated Security=True");
+            try
+            {
+                conn.Open();
+                string user = txtUsername.Text;
+                string pass = txtPassword.Text;
+                string sql = "SELECT * FROM dbo.log_in WHERE email ='" + user + "'and password ='" + pass + "';";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataReader dta = cmd.ExecuteReader();
+                if (dta.Read() == true)
+                {
+                    Form2 form2 = new Form2();
+                    this.Hide();
+                    form2.ShowDialog();
+                    this.Show();
+                }
+                else
+                {
+                    MessageBox.Show("Tên đăng nhập hoặc mật khẩu không chính xác", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Lỗi kết nối");
+            }
+        }
+
+        private void Cancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
